0d9450c530362eca5d61b9a02bb8bc8d1eb968db
katiecai <katiecai@g.ucla.edu>
2018-02-21 18:33:38 -0800

    hi jorge

--- HackSprint/ViewController.swift
+++ HackSprint/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 class ViewController: UIViewController {
 
@@ -20,6 +22,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func loadView() {
+        let camera = GMSCameraPosition.camera(withLatitude: 34.0689, longitude: -118.4452, zoom: 15.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 34.0689, longitude: -118.4452)
+        marker.title = "UCLA"
+        marker.map = mapView
+        
+    }
 
 }
 
